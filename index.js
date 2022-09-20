855c5ce4ce8ad28edb4bf7bbc55dfd636e3a1b13
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-09-20 14:24:31 +0100

    random function is done

---
+++ index.js
@@ -0,0 +1,26 @@
+let btnRandom = document.querySelector("button");
+let result = document.querySelector("h2");
+
+let users = [
+  "Marks",
+  "Kamal",
+  "Abdelhaq",
+  "Marouan",
+  "Amine",
+  "Mehdi",
+  "Mohammed",
+  "Mehdi",
+];
+
+function getRandomNumber(min, max) {
+    let step1 = max - min + 1;
+  let step2 = Math.random() * step1;
+  let step3 = Math.floor(step2);
+
+  return step3;
+}
+
+btnRandom.addEventListener("click", () => {
+  let index = getRandomNumber(0, users.length - 1);
+  result.innerText = users[index];
+});
