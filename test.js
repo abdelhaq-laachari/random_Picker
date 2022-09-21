cca31c325adc33b83a7d8f76264d957fe33ab4cf
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-09-21 21:34:24 +0100

    pick date is working fine

--- test.js
+++ test.js
@@ -1,65 +1,37 @@
-// var btn = document.getElementById("btn");
-
-// function getDatesInRange() {
-
-//   var today = new Date();
-//   var current_date =
-//   today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-//   console.log(current_date);
-
-//   const date = new Date(current_date.getTime());
-//   //   let DayName = "Wed";
-//   let DayName = date.toString().split(" ")[0];
-//   console.log("day Name:" + DayName);
-
-//   let nextDay = 0;
-//   var test = date.setDate(date.getDate() + nextDay);
-
-//   const dates = [];
-//   btn.addEventListener("click", () => {
-//     if (DayName === "Sat") {
-//       nextDay += 2;
-//       dates.push(new Date(date));
-//       console.log(nextDay);
-//     } else {
-//       nextDay = nextDay + 1;
-//       dates.push(new Date(date));
-//       console.log("NEXT DAY IS:" + nextDay);
-//       console.log(dates);
-//     }
-//   });
-
-//   // test
-//   // date.setDate(date.getDate() + 1);
-//   // nextDay++;
-
-//   return dates;
-// }
-
-// console.log(getDatesInRange());
+var btn = document.getElementById("btn");
 
 function getDatesInRange(startDate) {
   const date = new Date(startDate.getTime());
-  let DayName = date.toString().split(" ")[0];
-  console.log(DayName)
+  console.log(date);
 
-  // ✅ Exclude start date
-  date.setDate(date.getDate() + 1);
+  //   let DayName = "Wed";
 
   const dates = [];
 
-  // ✅ Exclude end date
-  while (date ) {
-    dates.push(new Date(date));
-    date.setDate(date.getDate() + 1);
-  }
+  btn.addEventListener("click", () => {
+    
+    var dayName = date.toString().split(" ")[0];
+
+    if (dayName != "Fri") {
+      var test = date.setDate(date.getDate() + 1);
+      dates.push(new Date(test));
+        console.log(dates);
+    } else {
+      var test = date.setDate(date.getDate() + 3);
+      dates.push(new Date(test));
+      console.log(dates);
+    }
+  });
 
   return dates;
 }
 
-var date = new Date();
+var today = new Date();
 var current_date =
-  date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+  today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
 
-  const d1 = new Date('2022-01-18');
+const d1 = new Date(current_date);
+
 console.log(getDatesInRange(d1));
+
+
